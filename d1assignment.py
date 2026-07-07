b115a20271d33e1d5cafac1edfdbaada6dbcf2a0
Aan Saji <a4aansaji@gmail.com>
2026-07-07 20:59:12 +0530

    Add files via upload

---
+++ d1assignment.py
@@ -0,0 +1,9 @@
+name = input("enter your name: ")
+age = input("enter your age: ")
+college = input("enter your college: ")
+branch = input("enter your branch: ")
+
+print(name)
+print(age)
+print(college)
+print(branch)
---
+++ day1.py
@@ -0,0 +1,22 @@
+print("hello, world"); 
+print("welcome to placement preparation")
+
+name = "Aan"
+age = "20"
+cgpa = "9.52"
+
+print("Name: " , name)
+print("Age:" , age)
+print("CGPA:" , cgpa)
+
+height = 164
+is_student = True
+
+print(type(height))
+print(type(is_student))
+
+name = input("enter your name")
+college = input("enter your collge")
+
+print(name)
+print(college)
