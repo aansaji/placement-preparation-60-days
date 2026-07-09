40df2e48739459e61b362dcafa37b32c1a05c880
Aan Saji <a4aansaji@gmail.com>
2026-07-09 19:23:28 +0530

    Add files via upload

---
+++ Day02_Operators_and_TypeCasting/assignment.py
@@ -0,0 +1,34 @@
+coins = 100
+
+print("Initial coins:", coins)
+
+coins += 50
+print("After earning 50:", coins)
+
+coins -= 30
+print("After spending 30:", coins)
+
+coins *= 2
+print("After bonus x2:", coins)
+
+coins /= 4
+print("After sharing with 4 players:", coins)
+
+
+
+
+
+wallet = 1000
+
+wallet += 500
+print("Balance after crediting 500 : ", wallet)
+
+wallet -= 300
+print("Balance after debiting 300 :", wallet)
+
+wallet *= 2
+print("Balance after wining price money:", wallet)
+
+wallet /= 2
+print("Balance after splitting money:", wallet)
+
