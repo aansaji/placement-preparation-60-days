8f1003757b4fc636fdb576e6c113237ee965fcd5
Aan Saji <a4aansaji@gmail.com>
2026-07-09 20:34:33 +0530

    Update type_casting.py

---
+++ Day02_Operators_and_TypeCasting/type_casting.py
@@ -0,0 +1,12 @@
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+
+print("Name:", name)
+print("Age after 5 years:", age + 5)
+
+food = int(input("Enter the price of food:"))
+delivery = int(input("Enter the delivery charge:"))
+
+Total = ( food+delivery )
+ 
+print("Total Bill: ", Total)
