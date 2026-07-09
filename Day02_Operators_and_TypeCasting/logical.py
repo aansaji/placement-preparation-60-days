7f64b67a62961d92af3d6314a226f22eed783911
Aan Saji <a4aansaji@gmail.com>
2026-07-09 19:37:30 +0530

    Update logical.py

---
+++ Day02_Operators_and_TypeCasting/logical.py
@@ -0,0 +1,14 @@
+age = 20
+has_id = True
+print("Can enter the club?", age>=18 and has_id)
+
+cgpa = 8
+attendance = 80
+print("Eligible for placements?", cgpa >= 7 and attendance >= 75)
+
+has_email = False
+has_phone = True
+print("Can user login?", has_email or has_phone)
+
+is_holiday = False
+print(not is_holiday)
